239ed7da3e75b59777630f700700534d7e63933e
nooboolean <nooboolean@i.softbank.jp>
2020-10-08 02:40:49 +0900

    Producerの実装

---
+++ src/main/java/com/example/kafkaProducerTest/Producer.java
@@ -0,0 +1,35 @@
+package com.example.kafkaProducerTest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+import java.lang.invoke.MethodHandles;
+
+@Component
+public class Producer {
+
+    private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    @Autowired
+    KafkaTemplate<String, String> template;
+
+    public void kafkaProducer(String key, String value){
+        log.info("SendingMessage: #{} => #{}", key, value);
+        template.send(key, value).addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+            @Override
+            public void onSuccess(SendResult<String, String> result){
+                log.info("SendResult: #{}", result);
+            }
+
+            @Override
+            public void onFailure(Throwable ex) {
+                log.error(ex.toString());
+            }
+        });
+    }
+}
--- src/main/java/com/example/kafkaProducerTest/kafkaProducerTestApplication.java
+++ src/main/java/com/example/kafkaProducerTest/kafkaProducerTestApplication.java
@@ -1,13 +1,34 @@
-package com.example.KafkaTest;
+package com.example.kafkaProducerTest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.lang.invoke.MethodHandles;
 
 @SpringBootApplication
 public class kafkaProducerTestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(kafkaProducerTestApplication.class, args);
+    private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    @Autowired
+    private Producer producer;
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext ctx = SpringApplication.run(kafkaProducerTestApplication.class, args);
+        try (ctx) {
+            kafkaProducerTestApplication app = ctx.getBean(kafkaProducerTestApplication.class);
+            app.run(args);
+        } catch (Exception ex) {
+            log.error(ex.toString());
+        }
 	}
+
+	public void run (String[] args) throws Exception{
+        producer.kafkaProducer("nakazawayTest", "テストです");
+    }
 
 }
